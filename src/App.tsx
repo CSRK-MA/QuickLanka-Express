62b8dabac4ca78c60a93e4c408867b1608455c2f
Shehan R.K <ruwanbrother1@gmail.com>
2025-08-13 07:24:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Changed import
 import { Toaster } from 'react-hot-toast';
 
 // Layouts
@@ -17,7 +17,7 @@ import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/QuickLanka-Express"> {/* Added basename */}
       <Toaster position="top-center" />
       <Routes>
         <Route path="/" element={<MainLayout />}>
